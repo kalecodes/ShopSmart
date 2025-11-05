317d6fe41955d5f37018945ef7add76f02dbdd6f
Joo Young Han <joha4022@gmail.com>
2025-11-04 20:19:36 -0800

    completed base template for landing-page

--- shop-smart-app/src/App.jsx
+++ shop-smart-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage.jsx";
 import NewAccountPage from "./pages/NewAccountPage.jsx";
+import SignInPage from "./pages/SignInPage.jsx";
 import AboutPage from "./pages/AboutPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import ShoppingPage from "./pages/ShoppingPage.jsx";
@@ -10,12 +11,13 @@ import Footer from "./components/Footer.jsx";
 export default function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
+      <div>
         <Navbar />
-        <main className="flex-grow">
+        <main>
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/new-account" element={<NewAccountPage />} />
+            <Route path="/sign-in" element={<SignInPage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/home" element={<HomePage />} />
             <Route path="/shopping" element={<ShoppingPage />} />
