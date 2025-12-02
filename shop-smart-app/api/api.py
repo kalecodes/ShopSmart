b5debe18c04d13706af1ec16542c6a9f7f2194ba
kalecodes <kalen.wiley@gmail.com>
2025-12-01 19:54:44 -0500

    api deployment setup

--- shop-smart-app/api/api.py
+++ shop-smart-app/api/api.py
@@ -1,4 +1,5 @@
 import time
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dbShopSmart import create
@@ -499,11 +500,13 @@ def login_user():
 
 ###############################################################################
 
+# if __name__ == "__main__":
+#     app.run(port=5001, debug=True)
+
+
 if __name__ == "__main__":
-    app.run(port=5001, debug=True)
-
-
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
